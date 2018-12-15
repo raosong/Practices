635a3f1904a8a3bfc91ee24cc81b7bfa25863583
Rao Song <raosong@outlook.com>
2018-12-14 16:11:38 -0800

    Added binary search

---
+++ Algorithms/BinarySearch/BinarySearch.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public class BinarySearch
+    {
+        public static int Search(int[] numbers, int target, int start, int end)
+        {
+            if (start > end)
+            {
+                return -1;
+            }
+
+            int mid = (start + end) / 2;
+            if (numbers[mid] == target)
+            {
+                return mid;
+            }
+            else
+            {
+                if (numbers[mid] > target)
+                {
+                    return Search(numbers, target, start, mid - 1);
+                }
+                else
+                {
+                    return Search(numbers, target, mid + 1, end);
+                }
+            }
+        }
+
+        public static int Search(int[] numbers, int target)
+        {
+            int start = 0;
+            int end = numbers.Length - 1;
+
+            while (start <= end)
+            {
+                int mid = (start + end) / 2;
+
+                if (numbers[mid] > target)
+                {
+                    end = mid - 1;
+                }
+                else
+                {
+                    if (numbers[mid] < target)
+                    {
+                        start = mid + 1;
+                    }
+                    else
+                    {
+                        return mid;
+                    }
+                }
+            }
+
+            return -1;
+        }
+    }
+}
--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -10,6 +10,14 @@ namespace Algorithms
     {
         static void Main(string[] args)
         {
+            //Binary Search Recursive
+            int[] numbers = new int[] { 2, 3, 7, 9, 10 };
+            int result = BinarySearch.Search(numbers, 9, 0, numbers.Length - 1);
+            Console.WriteLine("Recursive Result: {0}", result);
+
+            result = BinarySearch.Search(numbers, 9);
+            Console.WriteLine("Loop Result: {0}", result);
         }
+
     }
 }
