c4da9cf24a466e3d8bfa29ee39ee3444651f3a17
Rao Song <raosong@outlook.com>
2018-12-14 16:30:18 -0800

    added algorightms unit test

---
+++ Algorithms.Tests/BinarySearchTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Algorithms.Tests
+{
+    [TestClass]
+    public class BinarySearchTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
